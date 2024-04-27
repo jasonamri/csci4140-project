6aed307612e1444858a730cc4c0b91b0904b7b0c
jonweiwei <jonathanwei2013@gmail.com>
2024-04-27 11:22:37 +0800

    Registration page ui

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,5 +1,16 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import {
+    Grid,
+    Paper,
+    Avatar,
+    Stack,
+    Typography,
+    TextField,
+    Button,
+    Link,
+    Alert
+} from '@mui/material';
 
 const Register = () => {
     const [formData, setFormData] = useState({
@@ -27,13 +38,22 @@ const Register = () => {
 
     return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input type="text" placeholder="Username" name="username" value={username} onChange={onChange} required />
-                <input type="email" placeholder="Email" name="email" value={email} onChange={onChange} required />
-                <input type="password" placeholder="Password" name="password" value={password} onChange={onChange} required />
-                <button type="submit">Register</button>
-            </form>
-            {registrationResult && <p>{registrationResult}</p>}
+            <Grid>
+                <Paper elevation={10} sx={{ margin: '50px auto', height: '520px', width: '300px'}}>
+                    <Stack alignItems={'center'}>
+                        <Avatar sx={{ mt: '30px', width: '50px', height: '50px' }}></Avatar>
+                        <Typography variant='h4' sx={{ mt: '20px', mb: '20px' }}>Sign Up</Typography>
+                        <form onSubmit={onSubmit}>
+                            <TextField type="text" placeholder="Username" name="username" required value={username} onChange={onChange} sx={{ margin: "10px", width: "280px" }}></TextField>
+                            <TextField type="email" placeholder="Email" name="email" required value={email} onChange={onChange} sx={{ margin: "10px", width: "280px" }}></TextField>
+                            <TextField type="password" placeholder="Password" name="password" required value={password} onChange={onChange} sx={{ margin: "10px", width: "280px" }}></TextField>
+                            <Button variant="contained" type="submit" sx={{ display: "block", margin: "auto", mt: "10px", width: "100px" }}>Sign Up</Button>
+                        </form>
+                        <Typography sx={{ mt: '20px' }}>Already have an account? <Link href="/login">Log In</Link></Typography>
+                        {registrationResult && <Alert severity="info" sx={{ mt: '50px' }}>{registrationResult}</Alert>}
+                    </Stack>
+                </Paper>
+            </Grid>
         </div>
     );
 };
