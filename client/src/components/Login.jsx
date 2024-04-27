0ba07749f8eb5fb939a2d58a3a0b0af850bd6955
jonweiwei <jonathanwei2013@gmail.com>
2024-04-27 10:45:55 +0800

    Login page ui

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,19 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import {
+    Grid,
+    Paper,
+    Avatar,
+    Stack,
+    Typography,
+    TextField,
+    Button,
+    Link,
+    Alert
+} from '@mui/material';
+import LockPersonIcon from '@mui/icons-material/LockPerson';
+import './styles/Login.css';
 
 const Login = () => {
     const [formData, setFormData] = useState({
@@ -37,13 +50,22 @@ const Login = () => {
     };
 
     return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input type="text" placeholder="Username" name="username" value={username} onChange={onChange} required />
-                <input type="password" placeholder="Password" name="password" value={password} onChange={onChange} required />
-                <button type="submit">Login</button>
-            </form>
-            {loginResult && <p>{loginResult}</p>}
+        <div className='login'>
+            <Grid>
+                <Paper elevation={10} sx={{ margin: '50px auto', height: '450px', width: '300px'}}>
+                    <Stack alignItems={'center'}>
+                        <Avatar sx={{ mt: '30px', width: '50px', height: '50px' }}><LockPersonIcon></LockPersonIcon></Avatar>
+                        <Typography variant='h4' sx={{ mt: '20px', mb: '20px' }}>Log In</Typography>
+                        <form onSubmit={onSubmit}>
+                            <TextField type="text" placeholder="Username" name="username" required value={username} onChange={onChange} sx={{ margin: "10px", width: "280px" }}></TextField>
+                            <TextField type="password" placeholder="Password" name="password" required value={password} onChange={onChange} sx={{ margin: "10px", width: "280px" }}></TextField>
+                            <Button variant="contained" type="submit" sx={{ display: "block", margin: "auto", mt: "10px", width: "100px" }}>Login</Button>
+                        </form>
+                        <Typography sx={{ mt: '20px' }}>Don't have an account? <Link href="/register">Sign Up</Link></Typography>
+                        {loginResult && <Alert severity="info" sx={{ mt: '50px' }}>{loginResult}</Alert>}
+                    </Stack>
+                </Paper>
+            </Grid>
         </div>
     );
 };
